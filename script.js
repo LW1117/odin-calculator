5743c413debea65d574561d35a5e99c0a94c2e11
Aslam <93756533+LW1117@users.noreply.github.com>
2024-10-24 19:47:55 +0530

    Create calculate() to perform operations on numbers

---
+++ script.js
@@ -0,0 +1,14 @@
+calculate = (operand1, operand2, operator) => {
+  switch (operator) {
+    case "+":
+      return operand1 + operand2;
+    case "-":
+      return operand1 - operand2;
+    case "*":
+      return operand1 * operand2;
+    case "/":
+      return operand1 / operand2;
+    default:
+      break;
+  }
+};
