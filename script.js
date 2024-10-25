8643dafafef5324edd1bbd148cc0ea99dd07729f
Aslam <93756533+LW1117@users.noreply.github.com>
2024-10-25 22:14:16 +0530

    Add events for numbered buttons and to display them

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let firstOperand = 0;
 let secondOperand = 0;
 let operator = "";
 
+let displayValue = 0;
+
+const displayElement = document.getElementById("display-element");
+
+const numberButtons = document.querySelectorAll(".number-button");
+
 add = (firstOperand, secondOperand) => firstOperand + secondOperand;
 subtract = (firstOperand, secondOperand) => firstOperand - secondOperand;
 multiply = (firstOperand, secondOperand) => firstOperand * secondOperand;
@@ -13,3 +19,14 @@ operate = (firstOperand, secondOperand, operator) => {
   else if (operator == "*") return multiply(firstOperand, secondOperand);
   else if (operator == "/") return divide(firstOperand, secondOperand);
 };
+
+populateDisplay = (itemToDisplay) => {
+  displayElement.innerText = itemToDisplay;
+};
+
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", () => {
+    displayValue = numberButton.textContent;
+    displayElement.innerText = displayValue;
+  });
+});
