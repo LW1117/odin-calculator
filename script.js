22ee7d2112726afd414e23e1ca46a6ec874f30cf
Aslam <93756533+LW1117@users.noreply.github.com>
2024-10-25 21:56:45 +0530

    Rewrote script.js

--- script.js
+++ script.js
@@ -1,44 +1,15 @@
-calculate = (operand1, operand2, operator) => {
-  switch (operator) {
-    case "+":
-      return operand1 + operand2;
-    case "-":
-      return operand1 - operand2;
-    case "*":
-      return operand1 * operand2;
-    case "/":
-      return operand1 / operand2;
-    default:
-      break;
-  }
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = "";
+
+add = (firstOperand, secondOperand) => firstOperand + secondOperand;
+subtract = (firstOperand, secondOperand) => firstOperand - secondOperand;
+multiply = (firstOperand, secondOperand) => firstOperand * secondOperand;
+divide = (firstOperand, secondOperand) => firstOperand / secondOperand;
+
+operate = (firstOperand, secondOperand, operator) => {
+  if (operator == "+") return add(firstOperand, secondOperand);
+  else if (operator == "-") return subtract(firstOperand, secondOperand);
+  else if (operator == "*") return multiply(firstOperand, secondOperand);
+  else if (operator == "/") return divide(firstOperand, secondOperand);
 };
-
-const operand1Element = document.getElementById("operand1");
-const operand2Element = document.getElementById("operand2");
-
-const buttons = document.querySelectorAll(".button");
-const equalsBtn = document.getElementById("equals-btn");
-
-const resultDiv = document.getElementById("result");
-
-const resultsH1 = document.createElement("h1");
-
-operand1 = 0;
-operand2 = 0;
-operator = "";
-
-buttons.forEach((button) => {
-  if (button.value != "=")
-    button.addEventListener("click", () => {
-      operator = button.value;
-      // console.log(operator);
-    });
-});
-
-equalsBtn.addEventListener("click", () => {
-  operand1 = parseFloat(operand1Element.value);
-  operand2 = parseFloat(operand2Element.value);
-  result = calculate(operand1, operand2, operator);
-  resultsH1.innerText = result;
-  resultDiv.appendChild(resultsH1);
-});
